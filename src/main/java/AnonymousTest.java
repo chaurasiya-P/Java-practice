4559000888259e03dd2c27a179b0e6391a18e77f
chaurasiya-P <pradeep.chaurasiya183@gmail.com>
2025-06-05 21:39:38 +0530

    commit test

---
+++ src/main/java/AnonymousTest.java
@@ -0,0 +1,8 @@
+package main.java;
+
+public interface AnonymousTest {
+
+    void test1();
+    int test2(int number);
+
+}
---
+++ src/main/java/Practice.java
@@ -0,0 +1,91 @@
+package main.java;
+
+public class Practice {
+    public static void main(String[] args) {
+        System.out.println("Starting git testing");
+
+        System.out.println("test to print numbers in random order using two thread");
+        ThreadTest test = new ThreadTest();
+        ThreadTest test2 = new ThreadTest();
+        test.start();
+        test2.start();
+
+
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("test to print numbers in fixed order using two thread and having join on them");
+        try {
+            ThreadTest test3 = new ThreadTest();
+            ThreadTest test4 = new ThreadTest();
+            test3.start();
+            test3.join();
+            test4.join();
+            test4.start();
+        } catch (Exception e) {
+            System.out.println("Error in join");
+        }
+
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Runnable test for random order print");
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                for(int i=0;i<5;i++) {
+                    System.out.println("Runnable " + Thread.currentThread().getName() + " " + i);
+                }
+            }
+        };
+
+        //Runnable as functional interface
+        Runnable runnable2 = () -> {
+            for(int i=0;i<5;i++) {
+                System.out.println("Runnable as functional interface " + Thread.currentThread().getName() + " " + i);
+            }
+        };
+
+        Thread t1 = new Thread(runnable);
+        Thread t2 = new Thread(runnable2);
+        t1.start();
+        t2.start();
+
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        AnonymousTest anonymousTest = new AnonymousTest() {
+            @Override
+            public void test1() {
+                System.out.println("test for anonymous inner class");
+            }
+
+            @Override
+            public int test2(int n) {
+                return n*n;
+            }
+        };
+
+        anonymousTest.test1();
+        System.out.println("return: " + anonymousTest.test2(5));
+
+    }
+}
+
+class ThreadTest extends Thread{
+
+    @Override
+    public void run() {
+        for(int i = 0; i<5; i++) {
+            System.out.println(currentThread().getName() +" "+ i);
+        }
+    }
+}
